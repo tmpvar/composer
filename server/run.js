040377798b8ac99092a21300f83c4db6235edf75
Elijah Insua <tmpvar@gmail.com>
2010-10-23 17:03:04 -0400

    moving towars a more temporary storage solution for the time being, round 1

--- server/run.js
+++ server/run.js
@@ -1,128 +1,105 @@
 var connect    = require("connect"),
-    keys       = require("keys"),
-    store      = new keys.Memory(
     spawn      = require("child_process").spawn,
-    jsToPorts  = require("./port").jsToPorts;
+    jsToPorts  = require("./port").jsToPorts,
+    nodes      = {};
+
+function S4() {
+  return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+}
+function guid() {
+ return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
 
 connect.createServer.apply(connect, [
   connect.logger(),
   connect.bodyDecoder(),
   connect.router(function(app) {
-    app.get("/:name", function(req, res, next) {
-      store.get(req.params.name, function(err, data) {
-        if (err) {
-          res.writeHead(404, {'Content-type':'application/json'});
-          res.end(JSON.stringify({code: 404, body: "Not Found"}));
-          return;
-        }
-        res.writeHead(200, {'Content-type':'application/json'});
-        res.end(cursor.pop().toJSON(null, "  "));
-      });
-    });
-
-    app.get("/flows/:name/run", function(req, res, next) {
-      Flow.find({name: req.params.name}).one(function(cursor) {
-
-        if (!cursor || cursor.length === 0) {
-          res.writeHead(404, {"Content-type":"application/json"});
-          res.end(JSON.stringify({code: 404, body: "Not Found"}));
-        } else {
-          res.writeHead(200, {"Content-type":"text/plain"});
-          var child = spawn("/usr/local/bin/node",
-                           [__dirname + "/execute.js"]);
-
-          child.stdin.write(cursor.toJSON());
-          child.stdin.end();
-
-          child.stderr.on("data", function(data) {
-            console.log(data.toString());
-          });
-          child.stdout.on("data", function(data) {
-            res.write(data);
-          });
-          res.on("end", function() {
-
-          });
-          var done = function() {
-            try {
-              res.end();
-            } catch (e) {
-              console.log("the client disconnected");
-            }
-          };
-
-          child.stdout.on("end", done);
-          child.on("exit", done);
-
-        }
-      });
-    });
-
-    app.post("/flows", function(req, res, next) {
-      if (req.body) {
-        req.body.name = "THEFLOW";
-        var flow = new Flow(req.body, true);
-
-        flow.save(function() {
-          res.writeHead("201",{"Content-type" : "application/json"});
-          res.end('{ "status": 201}');
-        });
-      }
-    });
 
     app.get("/nodes", function(req, res, next) {
-      Node.find().all(function(cursor) {
-        res.writeHead(200, {"Content-type": "application/json"});
-        var result = [];
-        cursor.forEach(function(obj) {
-          result.push(obj.toObject());
-        });
-        res.end(JSON.stringify(result));
-      });
+      res.writeHead(200, {"Content-type": "application/json"});
+      res.end(JSON.stringify(nodes, null,  "  "));
     });
 
     app.post("/nodes", function(req, res, next) {
-      if (req.body) {
+      var body = req.body;
+      body.type = body.type || "js";
+      body.name = body.name || guid();
+
+
+      if (body && body.type && body.type !== "flow") {
         req.body.ports = jsToPorts(req.body.code);
-        var node = new Node(req.body, true);
-        node.save(function() {
-          res.writeHead("201", {"Content-type" : "application/json"});
-          res.end(JSON.stringify(node, null, true));
-        });
+      }
+
+      nodes[body.name] = body;
+      res.writeHead("201", {"Content-type" : "application/json"});
+      res.end(JSON.stringify(nodes[body.name], null, "  "));
+    });
+
+    app.get("/nodes/:name", function(req, res, next) {
+      var name = req.params.name.replace(/%20/g, " ");
+      if (!nodes[name]) {
+        res.writeHead(404, {"Content-type": "application/json"});
+        res.end(JSON.stringify({code: 404, body: "Not Found"}));
+      } else {
+        res.writeHead(200, {"Content-type": "application/json"});
+        res.end(JSON.stringify(nodes[name], null, "  "));
       }
     });
 
-    app.get("/nodes/:node", function(req, res, next) {
-      var node = req.params.node.replace("%20", " ");
-      Node.find({name: node}).one(function(cursor) {
-        if (!cursor || cursor.length === 0) {
+    app.put("/nodes/:name", function(req, res, next) {
+      if (req.body) {
+
+        var body = req.body,
+            name = req.params.name.replace(/%20/g, " ");
+        body.type = body.type || "js";
+        body.name = body.name || name;
+
+        if (!nodes[name]) {
           res.writeHead(404, {"Content-type": "application/json"});
           res.end(JSON.stringify({code: 404, body: "Not Found"}));
         } else {
-          res.writeHead(200, {"Content-type": "application/json"});
-          res.end(cursor.toJSON());
+          if (body.type !== "flow") {
+            body.ports = jsToPorts(body.code);
+          }
+          nodes[name] = body;
+          res.writeHead("200", {"Content-type" : "application/json"});
+          res.end(JSON.stringify(nodes[name], null, "  "));
         }
-      });
+      }
     });
 
-    app.put("/nodes/:node", function(req, res, next) {
-      if (req.body) {
-        var node = req.params.node.replace("%20", " ");
-        Node.find({name: node}).one(function(cursor) {
-          if (!cursor || cursor.length === 0) {
-            res.writeHead(404, {"Content-type": "application/json"});
-            res.end(JSON.stringify({code: 404, body: "Not Found"}));
-          } else {
-            var update = cursor.toObject();
-            console.dir(req.body)
-            update.code = req.body.code;
-            update.ports = jsToPorts(req.body.code);
-            (new Node(update, true)).save(function() {
-              res.writeHead("200", {"Content-type" : "application/json"});
-              res.end(JSON.stringify(update, null, true));
-            });
-          }
+    app.get("/nodes/:name/run", function(req, res, next) {
+      var name = req.params.name;
+      if (!nodes[name]) {
+        res.writeHead(404, {"Content-type":"application/json"});
+        res.end(JSON.stringify({code: 404, body: "Not Found"}));
+      } else {
+        res.writeHead(200, {"Content-type":"text/plain"});
+        var child = spawn("/usr/local/bin/node",
+                          [__dirname + "/execute.js"]);
+
+        child.stdin.write(JSON.stringify(nodes[name]));
+        child.stdin.end();
+
+        child.stderr.on("data", function(data) {
+          console.log(data.toString());
         });
+        child.stdout.on("data", function(data) {
+          res.write(data);
+        });
+        res.on("end", function() {
+
+        });
+        var done = function() {
+          try {
+            res.end();
+          } catch (e) {
+            console.log("the client disconnected");
+          }
+        };
+
+        child.stdout.on("end", done);
+        child.on("exit", done);
       }
     });
   }),
